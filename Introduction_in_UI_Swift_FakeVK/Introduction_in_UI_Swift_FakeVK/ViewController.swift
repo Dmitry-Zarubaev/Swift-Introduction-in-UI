b8d85ae256f2456694a4cd7198907c389196f67c
Dmitry Zarubaev <dmitry.zarubaev@area9.dk>
2022-02-14 14:02:12 +0300

    Create an xcode project

---
+++ Introduction_in_UI_Swift_FakeVK/Introduction_in_UI_Swift_FakeVK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Introduction_in_UI_Swift_FakeVK
+//
+//  Created by Дмитрий Зарубаев on 14.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
