02ad23bae5b08fe1218b22beb3a4efcbe5dea725
Dmitry Zarubaev <dmitry.zarubaev@area9.dk>
2022-02-23 17:58:33 +0300

    First working config

---
+++ Introduction_in_UI_Swift_FakeVK/Introduction_in_UI_Swift_FakeVK/FriendTableCell.swift
@@ -0,0 +1,16 @@
+//
+//  FriendCell.swift
+//  Introduction_in_UI_Swift_FakeVK
+//
+//  Created by Дмитрий Зарубаев on 23.02.2022.
+//
+
+import UIKit
+
+class FriendTableCell: UITableViewCell {
+
+    @IBOutlet var friendAvatarTableCell: UIImageView!
+    @IBOutlet var friendLabelTableCell: UILabel!
+    
+    
+}
